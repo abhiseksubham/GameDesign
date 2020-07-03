57f686dec676657e7612833c58a49b540edb934f
Abhisek Shubam <abhisekshubam@ml-laptop-034.local>
2020-07-03 13:44:32 +0530

    initial draft version

--- test.js
+++ test.js
@@ -1,30 +1,35 @@
-var children = [
-  { num: 6, name: 'me', phone: 7 },
-  { num: 8, name: 'him', phone: 9 },
-];
 let selected = [];
-createNRows = count => {
+let prices = 0;
+let noOfSeats = 1;
+let noOfselectedseats = 0;
+const alphabet = Array.from(Array(26), (e, i) => String.fromCharCode(i + 65));
+createNRows = (count, divname) => {
   for (let i = 0; i < count; i++) {
-    createNdivs(count, i, 'container');
+    createNdivs(15, i, divname);
   }
 };
 createNdivs = (count, start, parent) => {
   let a = document.createElement('div');
   a.className = 'parent';
+  let chardiv = document.createElement('div');
+  chardiv.innerHTML = alphabet[start];
+  chardiv.className = 'alphabets';
+  a.appendChild(chardiv);
   for (let i = 0; i < count; i++) {
     //console.log(i);
-    divid = start * 5 + i + 1;
+    divid = start * count + i + 1;
     let x = document.createElement('div');
-    x.innerHTML = divid;
-    x.className = 'initial';
-    x.id = divid;
+    x.innerHTML = i + 1;
+    if (start === 1 || start > 5) x.className = 'available';
+    else x.className = 'initial';
+    x.id = parent + divid;
     x.onclick = dynamicEvent;
     a.appendChild(x);
-    // htmlchunk += '<div class="initial">1</div>';
     document.getElementById(parent).appendChild(a);
   }
 };
-createNRows(5);
+createNRows(2, 'club');
+createNRows(8, 'executive');
 // createNdivs(8);
 function handleClick(i) {
   console.log('inhandleclick', i);
@@ -32,12 +37,34 @@ function handleClick(i) {
 
 function dynamicEvent(e) {
   var status = $(event.currentTarget).attr('id');
-  console.log('inDynamicEvent', status);
-  selected.push(status);
-  //   this.classList += 'active';
-  //   this.innerHTML = 'Dynamic event success.';
-  this.className += ' active';
-  console.log({ selected });
+  console.log('37', $(event.currentTarget).hasClass('available'));
+  if (
+    $(event.currentTarget).hasClass('available') &&
+    noOfselectedseats < noOfSeats
+  ) {
+    noOfselectedseats++;
+    console.log('inDynamicEvent', status);
+    selected.push(status);
+    //   this.classList += 'active';
+    //   this.innerHTML = 'Dynamic event success.';
+    this.className += ' active';
+    console.log({ selected });
 
-  createNdivs(selected.length, 0, 'final');
+    //createNdivs(selected.length, 0, 'final');
+    if (status.includes('executive')) {
+      prices += 200;
+    } else if (status.includes('club')) {
+      prices += 300;
+    }
+    $('.prices').text('Total:' + prices);
+  }
+}
+
+function setSeats() {
+  console.log(seats.value);
+  if (seats.value > 10) {
+    $('#seats').val(10);
+    return;
+  }
+  noOfSeats = seats.value;
 }
